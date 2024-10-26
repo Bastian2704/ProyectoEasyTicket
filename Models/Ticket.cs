ecea2ea951120182f61513a3a050c75b1e644151
Bastian2704 <sebastianabad2704@gmail.com>
2024-10-26 18:37:30 -0500

    Agregar archivos de proyecto.

---
+++ Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace ProyectoEasyTicket.Models
+{
+    public class Ticket
+    {
+        public int TicketID { get; set; }
+        public string? Evento { get; set; }
+        public DateTime? Fecha { get; set; }
+        public string? Lugar { get; set; }
+        public string? ButacaSeccion { get; set; }
+        public decimal? Precio { get; set; }
+
+        public string? Usuario { get; set; } 
+        public bool Vendido { get; set; }
+
+    }
+}
